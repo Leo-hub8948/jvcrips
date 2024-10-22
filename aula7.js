f58c70ad7593076313a780b2573322453fd6b68a
torlee <joaovictortorrestorle@gmail.com>
2024-10-22 20:48:50 -0300

    Leo
    nada

--- aula7.js
+++ aula7.js
@@ -1,17 +1,14 @@
-// OBJETO NO JAVASCRIPT.
-
 const Carta = {
-    nome: 'Naruto',
-    habilidade: 98,
-    defesa: 40,
-    ataque: 89,
-    slogan: 'naruto e razengam'
+    nome: "Baguko",
+    habilidade: 80,
+    defesa: 20,
+    ataque: 97
+    slogan: "Melhor em explosão"
 };
+
+
 console.log(`meu personagem se chama: ${Carta.nome}`);
 console.log(`Sua habilidade é: ${Carta.habilidade}`);
 console.log(`Sua defesa é ${Carta.defesa}`);
 console.log(`Seu ataque é ${Carta.ataque}`);
 console.log(`Mais conhecido como: ${Carta.slogan}`);
-
-
-// tipo objeto do JAVASCRIPT
---
+++ aula8.js
@@ -0,0 +1,5 @@
+const investimentoMensal = [1000, 2000, 3000, 4000, 5000, 6000];
+
+console.log(investimentoMensal);
+console.log(`O valor investido no mês de janeiro foi: ${investimentoMensal[5]}`);
+//ACESSO AO ÍDICE DO ELEMENTO ARRAY
