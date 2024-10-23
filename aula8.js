965e0721e748c569ab25520bcf330285264f0f1f
Wellington da Silva <nenem12@icloud.com>
2024-10-23 19:09:41 -0300

    Leo
    Timão

--- aula8.js
+++ aula8.js
@@ -3,3 +3,10 @@ const investimentoMensal = [1000, 2000, 3000, 4000, 5000, 6000];
 console.log(investimentoMensal);
 console.log(`O valor investido no mês de janeiro foi: ${investimentoMensal[5]}`);
 //ACESSO AO ÍDICE DO ELEMENTO ARRAY
+const numeros = [10, 20, 30, 40, 50];
+
+const valorNoIndicel = numeros[1];
+const valorNoIndice3 = numeros[3];
+
+const.log(`Valor no indice 1: ${valorNoIndicel}`);
+// Saída: 20 console.log(`Valor no índice 3: ${valorNoIndice3}`); // Saída: 40
---
+++ aula9.js
@@ -0,0 +1,10 @@
+const usuario = {
+    nome: 'Igor',
+    idade: 25,
+    time; 'Corinthians'
+};
+
+const mensagemDeBoasVindas1 = 'Bem vindo, torcedor tricolor!';
+const mensagemDeBoasVindas2 = 'bem vindo, torcedor Rubro-Negro';
+
+if (usuario.time ==)
