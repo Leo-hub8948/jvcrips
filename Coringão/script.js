1c02674281e97b575791bba726bf1304fab4e535
Miguel <1miguelarthur@gmail.com>
2024-11-04 20:54:02 -0300

    leo
    corinthians

---
+++ Coringão/script.js
@@ -0,0 +1,11 @@
+scrollReveal().reveal('#section01', {
+    duration: 1000,
+    origin: 'bottom',
+    distance: '50px',
+});
+
+scrollReveal().reveal('#section02', {
+    duration: 1000,
+    origin: 'right',
+    distance: '50px',
+});
---
+++ dupla/script.js
@@ -0,0 +1,17 @@
+ScrollReveal().reveal('#section01', {
+    duration: 1000,
+    origin: 'bottom',
+    distance: '50px',
+});
+
+ScrollReveal().reveal('#section02', {
+    duration: 1000,
+    origin: 'right',
+    distance: '50px',
+});
+
+ScrollReveal().reveal('#section03', {
+    duration: 1000,
+    origin: 'right',
+    distance: '50px',
+});
